58b24fdc166d5ad6804b70024661b6aa48706cb6
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-30 16:22:54 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ EmployeePyrolls/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePyrolls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Employee Payrolls");
+        }
+    }
+}
