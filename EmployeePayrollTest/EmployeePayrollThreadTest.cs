70339e18d62fc789c4b09119b514a30da0a5b4bf
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2021-01-05 18:59:53 +0530

    [vidyadhar hudge]Add:Test case for add multiple records to payroll table

--- EmployeePayrollTest/EmployeePayrollThreadTest.cs
+++ EmployeePayrollTest/EmployeePayrollThreadTest.cs
@@ -9,8 +9,11 @@ namespace EmployeePayrollTest
     [TestClass]
     public class EmployeePayrollThreadTest
     {
+        /// <summary>
+        /// given list and db when insert then calculate execution time
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void givenListAndDb_WhenInsert_ThenCalculateExacutionTime()
         {
             List<EmployeeModel> emploeeModellist = new List<EmployeeModel>();
             emploeeModellist.Add(new EmployeeModel() { EmployeeId = 12, EmployeeName = "Dhiraj", JobDescription = "Sale", Month = "may", EmployeeSalary =450000, SalaryId = 101, StartDate=new DateTime(2020, 01, 04), Gender = 'M',DepartmentId=3,CompanyId=2,isEmployeeActive=true});
@@ -27,7 +30,7 @@ namespace EmployeePayrollTest
             EmployeePayrollRepo employeePayrollRepo = new EmployeePayrollRepo();
             EmployeeModel employeeModel = new EmployeeModel
             {
-                EmployeeId = 106,
+                EmployeeId = 108,
                 EmployeeName = "amol",
                 JobDescription = "Hr",
                 Month = "oct",
@@ -43,6 +46,46 @@ namespace EmployeePayrollTest
             employeePayrollRepo.addEmployee(employeeModel);
             DateTime endTimeForDb = DateTime.Now;
             Console.WriteLine("Durations without the thread = " + (startTimeForDb - endTimeForDb));
+           
+            DateTime startTimeWithThread = DateTime.Now;
+            employeePayroll.addEmployeeToPayrollWithThread(emploeeModellist);
+            DateTime endTimeWithThread = DateTime.Now;
+            Console.WriteLine("Durations with the thread = " + (startTimeWithThread - endTimeWithThread));
+        }
+       
+        /// <summary>
+        ///given list and db when insert in payroll table then calculate execution time
+        /// </summary>
+        [TestMethod]
+        public void givenListAndDb_WhenInsertInPayrollTable_ThenCalculateExacutionTime()
+        {
+            List<EmployeeModel> emploeeModellist = new List<EmployeeModel>();
+            emploeeModellist.Add(new EmployeeModel() { EmployeeId = 1, EmployeeName = "Dhiraj", JobDescription = "Sale", Month = "may", EmployeeSalary = 450000, SalaryId = 101, StartDate = new DateTime(2020, 01, 04), Gender = 'M', DepartmentId = 3, CompanyId = 2, isEmployeeActive = true });
+            emploeeModellist.Add(new EmployeeModel() { EmployeeId = 2, EmployeeName = "suraj", JobDescription = "hr", Month = "nov", EmployeeSalary = 550000, SalaryId = 102, StartDate = new DateTime(2020, 02, 04), Gender = 'M', DepartmentId = 3, CompanyId = 2, isEmployeeActive = true });
+            emploeeModellist.Add(new EmployeeModel() { EmployeeId = 8, EmployeeName = "summet", JobDescription = "sale", Month = "dec", EmployeeSalary = 650000, SalaryId = 103, StartDate = new DateTime(2022, 01, 04), Gender = 'M', DepartmentId = 2, CompanyId = 2, isEmployeeActive = true });
+            emploeeModellist.Add(new EmployeeModel() { EmployeeId = 6, EmployeeName = "dharma", JobDescription = "Sale", Month = "oct", EmployeeSalary = 750000, SalaryId = 104, StartDate = new DateTime(2021, 01, 04), Gender = 'M', DepartmentId = 2, CompanyId = 3, isEmployeeActive = true });
+            emploeeModellist.Add(new EmployeeModel() { EmployeeId = 9, EmployeeName = "prem", JobDescription = "hr", Month = "apr", EmployeeSalary = 850000, SalaryId = 105, StartDate = new DateTime(2018, 01, 04), Gender = 'M', DepartmentId = 3, CompanyId = 2, isEmployeeActive = true });
+
+            EmployeePayrollOperation employeePayroll = new EmployeePayrollOperation();
+            DateTime startTime = DateTime.Now;
+            employeePayroll.addEmployeeToPayroll(emploeeModellist);
+            DateTime endTime = DateTime.Now;
+            Console.WriteLine("Durations without the thread = " + (endTime - startTime));
+            EmployeePayrollRepo employeePayrollRepo = new EmployeePayrollRepo();
+            EmployeeModel employeeModel = new EmployeeModel
+            {
+                EmployeeId = 107,
+                EmployeeName = "dhiraj",
+                JobDescription = "Hr",
+                Month = "may",
+                EmployeeSalary = 77000,
+                SalaryId = 101,
+                StartDate = new DateTime(2019, 02, 22),
+                Gender = 'M',
+                DepartmentId = 3,
+                CompanyId = 2,
+                isEmployeeActive = true
+            };
             
             DateTime startTimesForDb = DateTime.Now;
             employeePayrollRepo.addEmployeeToPayroll(employeeModel);
@@ -53,8 +96,6 @@ namespace EmployeePayrollTest
             employeePayroll.addEmployeeToPayrollWithThread(emploeeModellist);
             DateTime endTimeWithThread = DateTime.Now;
             Console.WriteLine("Durations with the thread = " + (startTimeWithThread - endTimeWithThread));
-
-
         }
     }
 }
