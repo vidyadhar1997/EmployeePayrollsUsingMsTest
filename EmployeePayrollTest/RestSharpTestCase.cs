9b955e279d09c9a1d4e476babe911a95785f85c2
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2021-01-07 14:05:33 +0530

    [vidyadhar hudge]Add:add new employee using rest api with post methode

--- EmployeePayrollTest/RestSharpTestCase.cs
+++ EmployeePayrollTest/RestSharpTestCase.cs
@@ -47,16 +47,35 @@ namespace RestSharpTestCase
         /// On the calling get API return employee list.
         /// </summary>
         [TestMethod]
-        public void onCallingGetApi_ReturnEmployeeList()
+        public void GivenonCalling_WhenGetApi_ThenReturnEmployeeList()
         {
             IRestResponse response = getEmployeeList();
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
             List<Employee> dataResorce = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(3, dataResorce.Count);
+            Assert.AreEqual(5, dataResorce.Count);
             foreach(Employee e in dataResorce)
             {
                 System.Console.WriteLine("id,: " + e.id + ",name:" + e.name + ",Salary:" + e.Salary);
             }
+        }
+        
+        /// <summary>
+        /// Ons the calling get API return employee list.
+        /// </summary>
+        [TestMethod]
+        public void GivenEmployee_WhenOnPost_ThenShouldReturnEmployee()
+        {
+            RestRequest request = new RestRequest("/employee", Method.POST);
+            JObject jObjectBody = new JObject();
+            jObjectBody.Add("name", "max");
+            jObjectBody.Add("Salary", "14748");
+            request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            Employee dataResorce = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("max", dataResorce.name);
+            Assert.AreEqual("14748", dataResorce.Salary);
+            System.Console.WriteLine(response.Content);
         }
     }
 }
